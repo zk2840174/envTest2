88df54df53f24df59d4185c020b51ca438741e63
zk202308A <zk202308A@gmail.com>
2024-12-06 15:06:44 +0900

    commit

--- src/main/java/org/zerock/envtest2/controller/SampleController.java
+++ src/main/java/org/zerock/envtest2/controller/SampleController.java
@@ -1,4 +1,30 @@
 package org.zerock.envtest2.controller;
 
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/sample")
 public class SampleController {
+
+
+
+    @Value("${org.zerock.V1}")
+    private String v1;
+
+    @Value("${org.zerock.V2}")
+    private String v2;
+
+
+    @GetMapping("/hello")
+    public String[] hello() {
+
+        return new String[] { v1, v2 };
+
+
+    }
+
 }
